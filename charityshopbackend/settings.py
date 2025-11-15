28e540bbe393ade9fba763f0cbf35c1eb0855427
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2025-11-15 03:31:27 -0600

    adjusts static root

--- charityshopbackend/settings.py
+++ charityshopbackend/settings.py
@@ -168,7 +168,7 @@ EMAIL_HOST_PASSWORD = 'vcfb xnbx nzim apkx'
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
