e589de6414bdca9434c2cbc50cccb2e15e36b104
Anthony Cowden <136389327+bluenote28@users.noreply.github.com>
2025-11-15 06:38:05 -0600

    adds amplify url to allowed origins

--- charityshopbackend/settings.py
+++ charityshopbackend/settings.py
@@ -178,5 +178,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
      "http://localhost:3000",
      "http://127.0.0.1:3000",
+     "https://main.d3ky9hapmr6avy.amplifyapp.com"
+    
 ]
 
