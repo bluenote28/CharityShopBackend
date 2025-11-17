ffd0966c7183bf4df269f97d1ff97c0410805059
anthonycowden <acowden@wgu.edu>
2025-11-17 08:59:14 -0600

    updates email app password

--- charityshopbackend/settings.py
+++ charityshopbackend/settings.py
@@ -162,7 +162,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'thecharityshopstore@gmail.com'
-EMAIL_HOST_PASSWORD = 'vcfb xnbx nzim apkx'
+EMAIL_HOST_PASSWORD = 'xhfp icag pnah yphn'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
