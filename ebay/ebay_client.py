f9d516d754e7a753fc04f7296be805a9091c25fe
anthonycowden <acowden@wgu.edu>
2025-11-25 03:08:42 -0600

    adds function to take secrets from environment variable and create a yaml file

--- ebay/ebay_client.py
+++ ebay/ebay_client.py
@@ -1,20 +1,35 @@
 from .oauthclient.oauth2api import oauth2api
 from .oauthclient.credentialutil import credentialutil
 from .oauthclient.model.model import environment
-import os, requests
+import os, requests, yaml
+from yaml import dump
 
 class EbayClient():
 
         def __init__(self, charity_ID):
                 self.charity_id = charity_ID
                 self.charity_url = f'https://api.ebay.com/buy/browse/v1/item_summary/search?limit=200&offset=200&charity_ids={charity_ID}'
+                self.yaml_file_path = os.path.join(os.path.split(__file__)[0],'ebay.yaml')
+
+        def __create_yaml_secrets(self):
+
+                secrets = {"api.ebay.com" : {"appid": os.environ.get('APP_ID'), 
+                                             "certid": os.environ.get('CERT_ID'),
+                                             "devid": os.environ.get('DEV_ID'),
+                                             "redirecturi": os.environ.get('REDIRECT_URI')}}
+                
+                with open(self.yaml_file_path, 'w') as file:
+                        yaml.dump(secrets, file, default_flow_style=False)
+
 
         def _get_ebay_token(self):
 
                 scopes = ['https://api.ebay.com/oauth/api_scope']
 
-                config_path = os.path.join(os.path.split(__file__)[0],'ebay.yaml')
-                credentialutil.load(config_path)
+                if os.path.exists(self.yaml_file_path) == False:
+                       self.__create_yaml_secrets()
+                       
+                credentialutil.load(self.yaml_file_path)
                 oauth2api_inst = oauth2api()
                 app_token = oauth2api_inst.get_application_token(environment.PRODUCTION, scopes)
 
