888611f4a6b5eddbab5f0c0aa0a246bdfd5d0c10
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2025-11-29 02:18:15 -0600

    adds report url

--- charityshopbackend/urls.py
+++ charityshopbackend/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     path('api/charity/', include('ebay.urls.charity_urls')),
     path('api/items/', include('ebay.urls.item_urls')),
     path('api/users/', include('ebay.urls.user_urls')),
+    path('api/report/', include('ebay.urls.report_urls')),
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name='reset_password.html'), name="reset_password.html"),
     path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'), name="password_reset_done"),
     path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='reset.html'), name="password_reset_confirm"),
