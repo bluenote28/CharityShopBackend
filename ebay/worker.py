8f6b04c37f3b65f76d99cc7903de47e0bdf69e0c
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2025-12-02 04:45:56 -0600

    manual redis config

--- ebay/worker.py
+++ ebay/worker.py
@@ -1,14 +1,14 @@
 import os
+from urllib.parse import urlparse
 import redis
 from rq import Worker, Queue, Connection
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
-
-conn = redis.from_url(redis_url)
+url = urlparse(os.environ.get("REDIS_URL"))
+r = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=(url.scheme == "rediss"), ssl_cert_reqs=None)
 
 if __name__ == '__main__':
-    with Connection(conn):
+    with Connection(r):
         worker = Worker(map(Queue, listen))
         worker.work()
