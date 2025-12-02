1b53706a995a2a387e81979c89a94d23fb7cd968
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2025-12-02 06:46:32 -0600

    puts django setup in try except

--- ebay/worker.py
+++ ebay/worker.py
@@ -6,19 +6,15 @@ from rq import Worker, Queue, Connection
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'charityshopbackend.settings')
 import django
 
-if django.apps.apps.ready:
-    listen = ['high', 'default', 'low']
-
-    url = urlparse(os.environ.get("REDIS_URL"))
-    conn = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=(url.scheme == "rediss"), ssl_cert_reqs=None)
-
-else:
+try:
     django.setup()
+except RuntimeError:
+    pass
 
-    listen = ['high', 'default', 'low']
+listen = ['high', 'default', 'low']
 
-    url = urlparse(os.environ.get("REDIS_URL"))
-    conn = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=(url.scheme == "rediss"), ssl_cert_reqs=None)
+url = urlparse(os.environ.get("REDIS_URL"))
+conn = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=(url.scheme == "rediss"), ssl_cert_reqs=None)
 
 if __name__ == '__main__':
     with Connection(conn):
