0f3c1ba876552e8e30b29a664b085e0a26719a7d
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2025-12-02 00:49:35 -0600

    changes to use redis

--- ebay/worker.py
+++ ebay/worker.py
@@ -1,11 +1,16 @@
 import os
-
 import redis
-from rq import Worker, Queue, Connection
+from redis import Redis
+from rq import Queue, Connection
+from rq.worker import HerokuWorker as Worker
+
 
 listen = ['high', 'default', 'low']
 
-redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
+redis_url = os.getenv('REDIS_URL')
+if not redis_url:
+    raise RuntimeError("Set up Heroku Data For Redis first, \
+    make sure its config var is named 'REDIS_URL'.")
 
 conn = redis.from_url(redis_url)
 
