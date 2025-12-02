6644b98944925e01199a50b21f001be74e0cde21
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2025-12-02 04:47:31 -0600

    changes redis connection variable name

--- ebay/worker.py
+++ ebay/worker.py
@@ -6,9 +6,9 @@ from rq import Worker, Queue, Connection
 listen = ['high', 'default', 'low']
 
 url = urlparse(os.environ.get("REDIS_URL"))
-r = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=(url.scheme == "rediss"), ssl_cert_reqs=None)
+conn = redis.Redis(host=url.hostname, port=url.port, password=url.password, ssl=(url.scheme == "rediss"), ssl_cert_reqs=None)
 
 if __name__ == '__main__':
-    with Connection(r):
+    with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
