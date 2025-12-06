1d2d9add67c4b0999d0ed2e5c3a79e81ce99c1e5
anthonycowden <acowden@wgu.edu>
2025-12-06 06:51:07 -0600

    adds exception handling

--- ebay/tasks.py
+++ ebay/tasks.py
@@ -1,9 +1,24 @@
 from django_rq import job
 
-@job
+
+charity = None
+
+@job('default', timeout=7200)
 def update_database(charity_id):
-        from .models import Charity
+
+    global charity 
+    charity = charity_id
+
+    try:
         from .load_data_to_db import DatabaseLoader
 
         loader = DatabaseLoader(charity_id)
+        print("loader created")
         loader.load_items_to_db()
+        print("loader finished")
+
+    except Exception as e:
+        print(f"Error updating database for charity {charity_id}: {e}")
+
+if charity is not None:
+    update_database.delay(charity)
