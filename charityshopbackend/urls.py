2b0de5cabb7e05e98a35f610eefaee1a33f35e31
anthonycowden <acowden@wgu.edu>
2025-12-10 03:14:46 -0600

    adds favorite list url

--- charityshopbackend/urls.py
+++ charityshopbackend/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from ebay.views.favorite_list import FavoriteListView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,6 +9,7 @@ urlpatterns = [
     path('api/items/', include('ebay.urls.item_urls')),
     path('api/users/', include('ebay.urls.user_urls')),
     path('api/report/', include('ebay.urls.report_urls')),
+    path('api/favorites/', FavoriteListView.as_view()),
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name='reset_password.html'), name="reset_password.html"),
     path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'), name="password_reset_done"),
     path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='reset.html'), name="password_reset_confirm"),
