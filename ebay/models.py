44f7954c9f062ea02fbe4c20c83bc37cd88d62e1
anthonycowden <acowden@wgu.edu>
2025-12-10 02:59:27 -0600

    adds favorite list model

--- ebay/models.py
+++ ebay/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Charity(models.Model):
     id = models.IntegerField(primary_key=True)
@@ -25,3 +26,15 @@ class Item(models.Model):
 
     def __str__(self):
         return self.name
+    
+class FavoriteList(models.Model):
+    id = models.AutoField(primary_key=True, )
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(Item, blank=True)
+    charities = models.ManyToManyField(Charity, blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"FavoriteList of User {self.user_id}"
