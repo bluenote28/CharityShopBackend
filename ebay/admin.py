fb9ca3c967707b497f9848f588f436528739f2b7
anthonycowden <acowden@wgu.edu>
2025-12-10 03:00:25 -0600

    adds favoriteList model

--- ebay/admin.py
+++ ebay/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Charity, Item
-
+from .models import Charity, Item, FavoriteList
 
 admin.site.register(Charity)
 admin.site.register(Item)
+admin.site.register(FavoriteList)
