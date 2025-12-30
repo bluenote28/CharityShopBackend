23068a43bb693993205b5c23ce19c48e8d307d91
anthonycowden <acowden@wgu.edu>
2025-12-30 13:37:01 -0600

    adds logging

--- charityshopbackend/settings.py
+++ charityshopbackend/settings.py
@@ -191,3 +191,23 @@ CORS_ALLOWED_ORIGINS = [
      "https://www.charityshopusa.com"   
 ]
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'INFO',
+            'propagate': False,
+        },
+    },
+}
