39fccf5148ccf9f2867eb38875bd0444f94f109d
anthonycowden <acowden@wgu.edu>
2025-12-30 11:49:53 -0600

    removes django-rq syntax

--- ebay/tasks.py
+++ ebay/tasks.py
@@ -1,13 +1,6 @@
-from django_rq import job
 
 
-charity = None
-
-@job('default', timeout=15000)
 def update_database(charity_id):
-
-    global charity 
-    charity = charity_id
 
     try:
         from .load_data_to_db import DatabaseLoader
@@ -19,6 +12,3 @@ def update_database(charity_id):
 
     except Exception as e:
         print(f"Error updating database for charity {charity_id}: {e}")
-
-if charity is not None:
-    update_database.delay(charity)
