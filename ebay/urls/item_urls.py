a0b3b3d037e10b23744f7185190d7a9908de9374
anthonycowden <acowden@wgu.edu>
2026-01-03 08:58:17 -0600

    adds single item url

--- ebay/urls/item_urls.py
+++ ebay/urls/item_urls.py
@@ -4,4 +4,5 @@ from ebay.views.item_views import EbayCharityItems
 
 urlpatterns = [
     path('ebaycharityitems/', EbayCharityItems.as_view()),
+    path('ebaycharityitems/<str:item_id>', EbayCharityItems.as_view()),
 ]
