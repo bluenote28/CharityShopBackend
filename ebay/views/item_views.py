d22d9b20ffbfe4008b080a05cd4d0b6774b4d393
anthonycowden <acowden@wgu.edu>
2026-01-03 08:55:22 -0600

    adds logic to handle a single item request

--- ebay/views/item_views.py
+++ ebay/views/item_views.py
@@ -2,12 +2,22 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from ebay.models import Item
 from ebay.serializers import ItemSerializer
-
+from databasescripts.database_actions import retrieveItem
 
 class EbayCharityItems(APIView):
     
-    def get(self, request):
+    def get(self, request, item_id=None):
 
-        items = Item.objects.all()
-        serializer = ItemSerializer(items, many=True)
-        return Response(serializer.data)
+        if item_id is not None:
+            item = retrieveItem(item_id)
+            if item is not None:
+                serializer = ItemSerializer(item)
+                return Response(serializer.data)
+            else:
+                return Response("Item not found", status=404)
+
+        else:
+
+            items = Item.objects.all()
+            serializer = ItemSerializer(items, many=True)
+            return Response(serializer.data)
