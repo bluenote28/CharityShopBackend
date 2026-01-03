5b07523dd3775dbd4fa346ba9c193c5d3036e65c
anthonycowden <acowden@wgu.edu>
2026-01-03 08:56:27 -0600

    removes unused imports

--- databasescripts/views.py
+++ databasescripts/views.py
@@ -1,12 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-import os
-import django_rq 
-import redis
 from rest_framework.permissions import IsAdminUser
 from ebay.tasks import update_database
 from django.db import close_old_connections
-from .database_actions import deleteCharity, addCharity
 from rq import Queue
 from ebay.worker import conn
 
