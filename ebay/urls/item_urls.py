75d847082894e9cc10aff428253334fd93ba4899
anthonycowden <acowden@wgu.edu>
2026-01-05 18:05:01 -0600

    add search and category urls

--- ebay/urls/item_urls.py
+++ ebay/urls/item_urls.py
@@ -3,6 +3,7 @@ from ebay.views.item_views import EbayCharityItems
 
 
 urlpatterns = [
-    path('ebaycharityitems/', EbayCharityItems.as_view()),
     path('ebaycharityitems/<str:item_id>', EbayCharityItems.as_view()),
+    path('ebaycharityitems/search/<str:search_text>', EbayCharityItems.as_view()),
+    path('ebaycharityitems/category/<str:category_id>', EbayCharityItems.as_view()),
 ]
