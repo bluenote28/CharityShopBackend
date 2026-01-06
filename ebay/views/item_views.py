d38dd3bbc7975c9c8d1fb8e19cb735b5ab19cd45
anthonycowden <acowden@wgu.edu>
2026-01-05 18:05:44 -0600

    adds logic to handle search and catergory requests

--- ebay/views/item_views.py
+++ ebay/views/item_views.py
@@ -2,11 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from ebay.models import Item
 from ebay.serializers import ItemSerializer
-from databasescripts.database_actions import retrieveItem
+from databasescripts.database_actions import retrieveItem, getItemsByCategory
 
 class EbayCharityItems(APIView):
     
-    def get(self, request, item_id=None):
+    def get(self, request, item_id=None, search_text=None, category_id=None):
 
         if item_id is not None:
             item = retrieveItem(item_id)
@@ -15,9 +15,16 @@ class EbayCharityItems(APIView):
                 return Response(serializer.data)
             else:
                 return Response("Item not found", status=404)
-
-        else:
-
-            items = Item.objects.all()
+            
+        elif search_text is not None:
+            items = Item.objects.filter(name__icontains=search_text)
             serializer = ItemSerializer(items, many=True)
             return Response(serializer.data)
+        
+        elif category_id is not None:
+            items = getItemsByCategory(category_id)
+            serializer = ItemSerializer(items, many=True)
+            return Response(serializer.data)
+
+        else:
+            return Response("Please provide an item_id, search_text, or category_id", status=400)
