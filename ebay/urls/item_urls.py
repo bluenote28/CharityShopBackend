d7a1460e758f5f7c9a7bb4b5a7268a749db18d88
anthonycowden <acowden@wgu.edu>
2026-01-14 05:01:28 -0600

    adds code to handle special characters in the category parameter

--- ebay/urls/item_urls.py
+++ ebay/urls/item_urls.py
@@ -1,9 +1,19 @@
-from django.urls import path
+from django.urls import path, register_converter
 from ebay.views.item_views import EbayCharityItems
 
+class CategoryWithSlashConverter:
+    regex = "[a-zA-Z,& /]+"
+
+    def to_python(self, value):
+        return str(value)
+
+    def to_url(self, value):
+        return value.replace('/', '%2F')
+
+register_converter(CategoryWithSlashConverter, "cat")
 
 urlpatterns = [
     path('ebaycharityitems/<str:item_id>', EbayCharityItems.as_view()),
     path('ebaycharityitems/search/<str:search_text>', EbayCharityItems.as_view()),
-    path('ebaycharityitems/category/<str:category_id>', EbayCharityItems.as_view()),
+    path('ebaycharityitems/category/<cat:category_id>', EbayCharityItems.as_view()),
 ]
