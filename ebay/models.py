96a236286df3cb7d825a72164e3194b8a8ddd85e
anthonycowden <acowden@wgu.edu>
2026-01-21 07:24:11 -0600

    adds url fileds for charity donation page and image url

--- ebay/models.py
+++ ebay/models.py
@@ -5,6 +5,8 @@ class Charity(models.Model):
     id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=100)
     description = models.TextField()
+    donation_url = models.URLField(null=True)
+    image_url = models.URLField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
