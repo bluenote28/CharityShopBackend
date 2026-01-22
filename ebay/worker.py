cbb89585e19e658bf6ce977e9fdd9cec7cf5f56b
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2026-01-22 01:38:26 -0600

    changes redis connection

--- ebay/worker.py
+++ ebay/worker.py
@@ -1,9 +1,10 @@
 import redis
+import os
 
 conn = redis.Redis(
     host='redis-14370.c244.us-east-1-2.ec2.cloud.redislabs.com',
     port=14370,
     decode_responses=True,
     username="default",
-    password="*******",
+    password=os.environ.get('REDIS_PASS'),
 )
