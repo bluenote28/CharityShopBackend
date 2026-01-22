c38d400e50ee6970215164a063ba7e49dd6d11d8
anthonycowden <acowden@wgu.edu>
2026-01-22 06:49:40 -0600

    changes to function

--- ebay/worker.py
+++ ebay/worker.py
@@ -1,7 +1,7 @@
 import os
 import redis
 
-
 REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379")
 
-conn = redis.from_url(REDIS_URL,ssl_cert_reqs=None)
+def get_redis():
+  return redis.from_url(REDIS_URL,ssl_cert_reqs=None)
