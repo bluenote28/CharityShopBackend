21d5eace7984d8e50200588c92dc923972b42aa9
Anthony Cowden <ancowden@gmail.com>
2026-01-27 22:55:22 -0600

    updates regex

--- ebay/urls/item_urls.py
+++ ebay/urls/item_urls.py
@@ -2,7 +2,7 @@ from django.urls import path, register_converter
 from ebay.views.item_views import EbayCharityItems
 
 class CategoryWithSlashConverter:
-    regex = "[a-zA-Z,& /-]+"
+    regex = "[a-zA-Z,& /-']+"
 
     def to_python(self, value):
         return str(value)
