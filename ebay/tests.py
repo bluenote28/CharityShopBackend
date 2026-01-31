cf81e41f14b46bd459ad9f39917ef629ba5d2be6
bluenote28 <136389327+bluenote28@users.noreply.github.com>
2026-01-31 01:35:07 -0600

    redoes tests for the item view

--- ebay/tests.py
+++ ebay/tests.py
@@ -1,4 +1,4 @@
-from django.test import TestCase, Client
+from django.test import TestCase, Client, RequestFactory
 from .models import *
 from django.contrib.auth.models import User
 from rest_framework.test import APIClient
@@ -6,6 +6,8 @@ from .ebay_client import EbayClient
 from unittest.mock import patch, Mock, MagicMock
 from .load_data_to_db import DatabaseLoader, WORD_FILTER
 import unittest
+from ebay.views.item_views import EbayCharityItems
+
 
 EBAY_CHARITY_URL = 'https://api.ebay.com/buy/browse/v1/item_summary/search?limit=200&offset=200&charity_ids=351044585'
 EBAY_CHARITY_ID = '351044585'
@@ -28,11 +30,24 @@ class CharityTestCase(TestCase):
 
 class ItemTestCase(TestCase):
     def setUp(self):
-        self.charity = Charity.objects.create(id=1, name="Test Charity", description="Test Description")
-        Item.objects.create(ebay_id="123456789", name="Test Item", img_url="http://test.com", web_url="http://test.com", price=10.00, charity=self.charity)
+        self.charity = Charity.objects.create(
+            id=1,
+            name="Test Charity",
+            description="Test Description"
+        )
+        self.item = Item.objects.create(
+            ebay_id="123456789",
+            name="Test Item",
+            img_url="http://test.com",
+            web_url="http://test.com",
+            price=10.00,
+            charity=self.charity
+        )
         self.client = Client()
+        self.factory = RequestFactory()
+        self.view = EbayCharityItems.as_view()
 
-    def test_item(self):
+    def test_item_model_fields(self):
         item = Item.objects.get(name="Test Item")
         self.assertEqual(item.name, "Test Item")
         self.assertEqual(item.ebay_id, "123456789")
@@ -41,10 +56,59 @@ class ItemTestCase(TestCase):
         self.assertEqual(item.price, 10.00)
         self.assertEqual(item.charity.name, "Test Charity")
 
-    def test_item_view(self):
+    @patch('ebay.views.item_views.retrieveItem')
+    def test_get_item_by_id_found(self, mock_retrieve):
+        mock_retrieve.return_value = self.item
         response = self.client.get('/api/items/ebaycharityitems/123456789')
+        
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Test Item")
+        mock_retrieve.assert_called_once_with('123456789')
+
+    @patch('ebay.views.item_views.retrieveItem')
+    def test_get_item_by_id_not_found(self, mock_retrieve):
+        mock_retrieve.return_value = None
+        response = self.client.get('/api/items/ebaycharityitems/999999999')
+        
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(response.data, "Item not found")
+
+    def test_search_items_with_results(self):
+        response = self.client.get('/api/items/ebaycharityitems/search/Test')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('results', response.data)
+
+    def test_search_items_no_results(self):
+        response = self.client.get('/api/items/ebaycharityitems/search/NonExistentItem')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.data['results'], [])
+
+    @patch('ebay.views.item_views.getItemsBySubCategory')
+    def test_get_items_by_category_with_results(self, mock_get_category):
+        mock_get_category.return_value = Item.objects.all()
+        response = self.client.get('/api/items/ebaycharityitems/category/Electronics')
+        
+        self.assertEqual(response.status_code, 200)
+        mock_get_category.assert_called_once_with('Electronics')
+        self.assertIn('results', response.data)
+
+    @patch('ebay.views.item_views.getItemsBySubCategory')
+    def test_get_items_by_category_empty(self, mock_get_category):
+        mock_get_category.return_value = Item.objects.none()
+        response = self.client.get('/api/items/ebaycharityitems/category/EmptyCategory')
+        
+        self.assertEqual(response.status_code, 200)
+        mock_get_category.assert_called_once_with('EmptyCategory')
+
+    def test_no_parameters_returns_400(self):
+        request = self.factory.get('/fake-url/')
+        response = self.view(request)
+        
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(
+            response.data,
+            "Please provide an item_id, search_text, or category_id"
+        )
 
 class TestTokenGeneration(TestCase):
     def setUp(self):
