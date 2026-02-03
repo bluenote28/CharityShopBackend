c49dbdac3c75a230635a648e5e8ff80d5479941d
Anthony Cowden <ancowden@gmail.com>
2026-02-03 02:22:37 -0600

    adds logic to handle filtering option

--- ebay/views/item_views.py
+++ ebay/views/item_views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from ebay.models import Item
 from ebay.serializers import ItemSerializer
-from databasescripts.database_actions import retrieveItem, getItemsBySubCategory
+from databasescripts.database_actions import retrieveItem, getItemsBySubCategory, getItemsByFilter
 from rest_framework.pagination import PageNumberPagination
 
 class EbayCharityItems(APIView):
@@ -10,7 +10,7 @@ class EbayCharityItems(APIView):
     paginator = PageNumberPagination()
     paginator.page_size = 50 
     
-    def get(self, request, item_id=None, search_text=None, category_id=None):
+    def get(self, request, item_id=None, search_text=None, category_id=None, filter=None):
 
         if item_id is not None:
             item = retrieveItem(item_id)
@@ -27,10 +27,17 @@ class EbayCharityItems(APIView):
             return self.paginator.get_paginated_response(serializer.data)
         
         elif category_id is not None:
-            items = getItemsBySubCategory(category_id)
-            paginated_items = self.paginator.paginate_queryset(items, request, self)
-            serializer = ItemSerializer(paginated_items, many=True)
-            return self.paginator.get_paginated_response(serializer.data)
+
+           if filter is None: 
+                items = getItemsBySubCategory(category_id)
+                paginated_items = self.paginator.paginate_queryset(items, request, self)
+                serializer = ItemSerializer(paginated_items, many=True)
+                return self.paginator.get_paginated_response(serializer.data)
+           else:
+                items = getItemsByFilter(category_id, filter)
+                paginated_items = self.paginator.paginate_queryset(items, request, self)
+                serializer = ItemSerializer(paginated_items, many=True)
+                return self.paginator.get_paginated_response(serializer.data)
 
         else:
             return Response("Please provide an item_id, search_text, or category_id", status=400)
