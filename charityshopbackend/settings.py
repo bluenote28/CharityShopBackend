3781e7718e41486294658cf55d0ae1254eb59059
Anthony Cowden <ancowden@gmail.com>
2026-02-24 04:34:26 -0600

    adds checkout app

--- charityshopbackend/settings.py
+++ charityshopbackend/settings.py
@@ -29,7 +29,8 @@ INSTALLED_APPS = [
     'corsheaders',
     'ebay.apps.EbayConfig',
     'django_rest_passwordreset', 
-    'databasescripts.apps.DatabasescriptsConfig'
+    'databasescripts.apps.DatabasescriptsConfig',
+    'checkout.apps.CheckoutConfig'
 ]
 
 REST_FRAMEWORK = {
