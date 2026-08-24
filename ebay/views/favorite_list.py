bb5d13d7ebabd3ac3a37713a2fd539c464fdc254
Anthony Cowden <ancowden@gmail.com>
2026-08-24 02:00:35 -0500

    restructured for new front end implementation

--- ebay/views/favorite_list.py
+++ ebay/views/favorite_list.py
@@ -1,19 +1,26 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from ebay.models import FavoriteList, Item, User
-from ebay.serializers import FavoriteListSerializer, CharitySerializer, ItemSerializer
+from ebay.serializers import FavoriteListSerializer, CharitySerializer
 
 class FavoriteListView(APIView):
+
+    def _favorite_field(self, request, field):
+        data = request.data or {}
+        value = data.get(field)
+        if value in (None, ''):
+            value = request.query_params.get(field)
+        return value or None
     
     def get(self, request):
         user = User.objects.get(username=request.user)
-        favorite_list = FavoriteList.objects.get(user=user.id)
+        favorite_list = FavoriteList.objects.prefetch_related('items', 'charities').get(user=user.id)
         serializer = FavoriteListSerializer(favorite_list, many=False)
         return Response(serializer.data)
     
     def post(self, request):
         data = request.data  
-        favorite_list = FavoriteList.objects.get(user=request.user)
+        favorite_list = FavoriteList.objects.prefetch_related('items', 'charities').get(user=request.user)
 
         if data['item'] != "":
             item = Item.objects.get(ebay_id=data['item'])
@@ -31,16 +38,17 @@ class FavoriteListView(APIView):
     
     def delete(self, request):
         user = request.user
-        data = request.data
+        item_id = self._favorite_field(request, 'item')
+        charity_id = self._favorite_field(request, 'charity')
         
-        favorite_list = FavoriteList.objects.get(user=user)
+        favorite_list = FavoriteList.objects.prefetch_related('items', 'charities').get(user=user)
 
-        if 'item' in data:
-            item = Item.objects.get(ebay_id=data['item'])
+        if item_id:
+            item = Item.objects.get(ebay_id=item_id)
             favorite_list.items.remove(item)
 
-        if 'charity' in data:
-            favorite_list.charities.remove(data['charity'])
+        if charity_id:
+            favorite_list.charities.remove(charity_id)
 
         favorite_list.save()
         serializer = FavoriteListSerializer(favorite_list, many=False)
