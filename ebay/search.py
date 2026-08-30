1a58043c4528f7f91ccd6ecd7a50bb3fd23b0099
Anthony Cowden <ancowden@gmail.com>
2026-08-30 04:37:23 -0500

    edits vector searches

--- ebay/search.py
+++ ebay/search.py
@@ -1,8 +1,22 @@
 from ebay.models import Item
+from ebay.constants import FILTER_OPTIONS
 from django.contrib.postgres.search import SearchVector,SearchQuery
 
-def search(query):
+def inFilterOptions(query):
+    return query.title() in FILTER_OPTIONS.keys()
 
+def search(query):
     search_query = SearchQuery(query, search_type='plain')
-        
-    return Item.objects.annotate(search=SearchVector('name', 'seller_description', 'category')).filter(search=search_query)
+
+    if inFilterOptions(query):
+        name_vector = SearchVector('name', weight='B')
+        category_vector = SearchVector('category', weight='A')
+        description_vector = SearchVector('seller_description', weight='C')
+    else:
+        name_vector = SearchVector('name', weight='B')
+        category_vector = SearchVector('category', weight='C')
+        description_vector = SearchVector('seller_description', weight='A')
+
+    search_vector = name_vector + category_vector + description_vector
+
+    return Item.objects.annotate(search=search_vector).filter(search=search_query)
