17f31c38cfdbe416bc0d38bea825bbf9d4798199
Anthony Cowden <ancowden@gmail.com>
2026-08-30 05:18:17 -0500

    not needed

--- databasescripts/management/commands/delete_inactive_items.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from databasescripts.delete_inactive_items import deleteInactiveItems
-
-
-class Command(BaseCommand):
-    help = "Delete eBay items that have been inactive for more than 14 days"
-
-    def handle(self, *args, **options):
-        deleteInactiveItems()
