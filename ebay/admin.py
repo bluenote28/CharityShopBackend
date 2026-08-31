8ce8082cf96ad097ffdf72e2596a9db3c51ce48f
Anthony Cowden <ancowden@gmail.com>
2026-08-31 01:48:12 -0500

    registers purchase model

--- ebay/admin.py
+++ ebay/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Charity, Item, FavoriteList
+from .models import Charity, Item, FavoriteList, Purchase
 
 admin.site.register(Charity)
 admin.site.register(Item)
 admin.site.register(FavoriteList)
+admin.site.register(Purchase)
