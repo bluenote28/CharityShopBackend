2fcad292c822735c4926b993fc2d0851d8a6d5e3
Anthony Cowden <ancowden@gmail.com>
2026-09-11 02:31:39 -0500

    changes settings

--- run_worker.py
+++ run_worker.py
@@ -1,7 +1,7 @@
 import os
 import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'charityshopbackend.settings')
 django.setup()
 
 from redis import Redis
