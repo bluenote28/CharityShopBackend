ed2bbd39764cdc1487de8027cdbdf16cb6ac184e
Anthony Cowden <ancowden@gmail.com>
2026-09-12 04:15:38 -0500

    adds account urls

--- charityshopbackend/urls.py
+++ charityshopbackend/urls.py
@@ -18,5 +18,6 @@ urlpatterns = [
     path("password_reset/", auth_views.PasswordResetView.as_view(template_name='reset_password.html'), name="reset_password.html"),
     path("reset_password_sent/", auth_views.PasswordResetDoneView.as_view(template_name='reset_password_sent.html'), name="password_reset_done"),
     path("reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name='reset.html'), name="password_reset_confirm"),
-    path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(template_name='reset_password_complete.html'), name='password_reset_complete')
+    path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(template_name='reset_password_complete.html'), name='password_reset_complete'),
+    path('accounts/', include('allauth.urls')),
 ]
