69022d16bf24784140a72ac48ab89376d2a27e34
Anthony Cowden <ancowden@gmail.com>
2026-09-13 02:11:16 -0500

    changes to use new ai client

--- aiassistant/views.py
+++ aiassistant/views.py
@@ -1,17 +1,19 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from .bedrock_client import get_item_description
+from rest_framework import status
+from .ai_client import get_item_description
 
 class AiItemAssistantView(APIView):
 
-    def __init__(self):
-        super().__init__()
-    
     def post(self, request):
-      
-        item_name = request.data['item_name']
+        ebay_id = request.data.get('ebay_id')
+        if not ebay_id:
+            return Response({'detail': 'Missing eBay ID'}, status=status.HTTP_400_BAD_REQUEST)
 
-        response = get_item_description(item_name)
-
-        return Response(response)
-    
+        item_link = request.data.get('item_link') or f'https://www.ebay.com/itm/{ebay_id}'
+        result = get_item_description(
+            item_link,
+            item_name=request.data.get('item_name'),
+            ebay_id=ebay_id,
+        )
+        return Response(result)
