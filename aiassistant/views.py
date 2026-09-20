91dde9d835a698ca5e0c3a46cbd3a48cf8fce85a
Anthony Cowden <ancowden@gmail.com>
2026-09-19 19:34:21 -0500

    rewritten for new function

--- aiassistant/views.py
+++ aiassistant/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from .ai_client import get_item_description
+from .ai_client import get_ai_advice
 
 class AiItemAssistantView(APIView):
 
@@ -10,10 +10,5 @@ class AiItemAssistantView(APIView):
         if not ebay_id:
             return Response({'detail': 'Missing eBay ID'}, status=status.HTTP_400_BAD_REQUEST)
 
-        item_link = request.data.get('item_link') or f'https://www.ebay.com/itm/{ebay_id}'
-        result = get_item_description(
-            item_link,
-            item_name=request.data.get('item_name'),
-            ebay_id=ebay_id,
-        )
+        result = get_ai_advice(ebay_id)
         return Response(result)
