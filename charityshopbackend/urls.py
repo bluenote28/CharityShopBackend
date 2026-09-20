d6d27747dad6755efbb8cda587a174fe3a0080e9
Anthony Cowden <ancowden@gmail.com>
2026-09-20 02:07:05 -0500

    adss endpoint for ai assistant

--- charityshopbackend/urls.py
+++ charityshopbackend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from ebay.views.favorite_list import FavoriteListView
 from databasescripts.views import RefreshDatabaseView
-from aiassistant.views import AiItemAssistantView
+from aiassistant.views import AiItemAssistantView, AiChatView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,6 +12,7 @@ urlpatterns = [
     path('api/report/', include('ebay.urls.report_urls')),
     path('api/purchases/', include('ebay.urls.purchase_urls')),
     path('api/favorites/', FavoriteListView.as_view()),
+    path('api/ai_assistant/chat/', AiChatView.as_view()),
     path('api/ai_assistant/', AiItemAssistantView.as_view()),
     path('api/refresh_items/', RefreshDatabaseView.as_view()),
     path('accounts/', include('allauth.urls')),
