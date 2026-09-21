2f8cdd90692908a2aa974f96a751a223498fade1
Anthony Cowden <ancowden@gmail.com>
2026-09-21 00:47:49 -0500

    adds get charity tool

--- aiassistant/tools.py
+++ aiassistant/tools.py
@@ -1,5 +1,9 @@
+import json
 import requests
 from bs4 import BeautifulSoup
+from django.core.serializers.json import DjangoJSONEncoder
+from ebay.models import Charity
+from ebay.serializers import CharitySerializer
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"}
 
@@ -8,7 +12,13 @@ def checkMysticMonk():
    response = requests.get(mystic_monk_coffee_url, headers=headers)
    soup = BeautifulSoup(response.text, "html.parser")
    coffee = soup.find_all("product-card", class_="product-card")
-   print(coffee)
+   ##coffee_names  = [coffee.find("div", class_="coffee-title").text for coffee in coffee]
+   coffee_names = soup.find_all("div", class_="coffee-title")
+   print(coffee_names)
+
+def get_all_charities():
+   data = CharitySerializer(Charity.objects.all(), many=True).data
+   return json.dumps(list(data), cls=DjangoJSONEncoder)
 
 if __name__ == "__main__":
    checkMysticMonk()
