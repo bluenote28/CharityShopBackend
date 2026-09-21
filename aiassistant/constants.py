d310dced5088f43d8b2c542fc6c62d88fed3a34e
Anthony Cowden <ancowden@gmail.com>
2026-09-21 00:47:25 -0500

    initial commit

---
+++ aiassistant/constants.py
@@ -0,0 +1,6 @@
+get_all_charities_function = {
+    "name": "get_all_charities",
+    "description": "Retrieve all charities from the database.",
+}
+
+assistant_tools = [{"type": "function", "function": get_all_charities_function}]
