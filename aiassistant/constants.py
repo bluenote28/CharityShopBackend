66e39ffc4fdace03b04096a01385fb6f90562bec
Anthony Cowden <ancowden@gmail.com>
2026-09-22 04:20:38 -0500

    adds search tool

--- aiassistant/constants.py
+++ aiassistant/constants.py
@@ -3,4 +3,19 @@ get_all_charities_function = {
     "description": "Retrieve all charities from the database.",
 }
 
-assistant_tools = [{"type": "function", "function": get_all_charities_function}]
+search_function = {
+    "name": "search_items",
+    "description": "Search for a product on the website.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "query": {"type": "string", "description": "The search query."},
+            "charity_id": {"type": "string", "description": "The charity id of the charity to filter by."},
+            "category": {"type": "string", "description": "The category of the product to filter by."},
+            "charity_ids": {"type": "array", "description": "The charity ids of the charities to filter by."},
+        },
+        "required": ["query"],
+    },
+}
+
+assistant_tools = [{"type": "function", "function": get_all_charities_function}, {"type": "function", "function": search_function}]
